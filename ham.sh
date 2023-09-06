dbaa1062a2a6d8310417d74ceae887661f4311e1
_N3m0 <n3m0o7@hotmail.com>
2023-09-06 17:32:25 +0200

    clean

--- ham.sh
+++
@@ -1,3 +0,0 @@
-g++ -Wall -Wextra -Iinclude -Ilib -Og -g -MP -MD -DDEBUG=1 -o build/te.o -c te.cpp
-g++ -o build/te build/te.o build/glad.l build/stb_image_imp.l lib/libglfw3.a -Wall -Wextra -Iinclude -Ilib -Og -g -MP -MD -DDEBUG=1 -Llib -lX11
-./build/te
